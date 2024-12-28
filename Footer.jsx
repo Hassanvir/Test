918d505f9c12f320d8f6825686a081b12498bb9a
Hassanvir <hassanvirsingh5@gmail.com>
2024-12-28 04:19:21 -0700

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p> copyright 2024 edusity. All right reserved.</p>
+        <ul>
+            <li>Terms of services</li>
+            <li>Privacy polices</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Footer
